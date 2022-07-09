c71f791adfbbe233c7858963f97153c1dbd00f4b
Konstantin Avdeev <fujiun91@gmail.com>
2022-07-09 18:42:56 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+
+    public static void main(String[] args) {
+        System.out.println("Konstantin Avdeev");
+        System.out.println("09.09.1991");
+    }
+
+}
